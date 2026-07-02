e8146f05ada212301f3ef6be49dba5101106dccd
Rafif Aran Kusumo <apiparan17@gmail.com>
2026-07-02 16:35:15 +0700

    modified:   js-script.js

--- js-script.js
+++ js-script.js
@@ -1,63 +1,63 @@
 const data = {
-logo:"/img/FS-logo.png",
-heroImage:"/img/general-placeholder.png",
+logo:"img/FS-logo.png",
+heroImage:"img/general-placeholder.png",
 socials:[
 {
-icon:"/img/IG-logo.png",
+icon:"img/IG-logo.png",
 link:"https://www.instagram.com/fuelstop.smoothies"
 },
 {
-icon:"/img/gopud-logo.png",
+icon:"img/gopud-logo.png",
 link:"https://gofood.co.id/en"
 },
 {
-icon:"/img/WA-logo.png",
+icon:"img/WA-logo.png",
 link:"https://wa.me/628871827081" //
 }
 ],
 products:[
 {
 name:"Green Energy",
-image:"/img/general-placeholder.png",
+image:"img/general-placeholder.png",
 description:"Smoothie berbahan sayuran hijau dan buah segar untuk membantu menjaga energi dan mendukung aktivitas harian.",
 nutrition:["High fiber","Vitamin A","Vitamin C","Low fat"]
 },
 {
 name:"Berry Boost",
-image:"/img/general-placeholder.png",
+image:"img/general-placeholder.png",
 description:"Smoothie dengan kombinasi buah berry alami sebagai pilihan meal replacement yang praktis dan menyegarkan.",
 nutrition:["Antioxidant","High vitamin","Natural sweetness","Rich in minerals"]
 },
 {
 name:"Tropical Mix",
-image:"/img/general-placeholder.png",
+image:"img/general-placeholder.png",
 description:"Perpaduan buah tropis segar dengan rasa ringan untuk menemani aktivitas sehari-hari.",
 nutrition:["Vitamin C","Natural fruit nutrients","Source of energy","Contains fresh fruit fiber"]
 }
 ],
 team:[
 {
-photo:"/img/PH-CEO.png",
+photo:"img/PH-CEO.png",
 name:"Muhammad Rafi Kanza",
 role:"CEO"
 },
 {
-photo:"/img/PH-Finance.png",
+photo:"img/PH-Finance.png",
 name:"Muhammad Iksan Hakiki",
 role:"Financial Manager"
 },
 {
-photo:"/img/PH-Marketing.png",
+photo:"img/PH-Marketing.png",
 name:"Mochamad Windu Gamara Akuba",
 role:"Marketing Manager"
 },
 {
-photo:"/img/PH-Production.png",
+photo:"img/PH-Production.png",
 name:"Rafif Aran Kusumo",
 role:"Production Manager"
 },
 {
-photo:"/img/PH-CS.png",
+photo:"img/PH-CS.png",
 name:"Yanti Dewi Lestari",
 role:"Customer Service"
 }
